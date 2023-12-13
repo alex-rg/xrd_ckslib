527eb31d84045fbea4fb557628def8f408f7919c
Alexander Rogovskiy <alexander.rogovskiy@gmail.com>
2023-12-13 11:25:52 +0000

    Add first version of the custom checksum manager

---
+++ MyXrdCksManager.cc
@@ -0,0 +1,126 @@
+/******************************************************************************/
+/*                                                                            */
+/*                   X r d O s s C k s M a n a g e r . c c                    */
+/*                                                                            */
+/* (c) 2011 by the Board of Trustees of the Leland Stanford, Jr., University  */
+/*                            All Rights Reserved                             */
+/*   Produced by Andrew Hanushevsky for Stanford University under contract    */
+/*              DE-AC02-76-SFO0515 with the Department of Energy              */
+/*                                                                            */
+/* This file is part of the XRootD software suite.                            */
+/*                                                                            */
+/* XRootD is free software: you can redistribute it and/or modify it under    */
+/* the terms of the GNU Lesser General Public License as published by the     */
+/* Free Software Foundation, either version 3 of the License, or (at your     */
+/* option) any later version.                                                 */
+/*                                                                            */
+/* XRootD is distributed in the hope that it will be useful, but WITHOUT      */
+/* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or      */
+/* FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public       */
+/* License for more details.                                                  */
+/*                                                                            */
+/* You should have received a copy of the GNU Lesser General Public License   */
+/* along with XRootD in a file called COPYING.LESSER (LGPL license) and file  */
+/* COPYING (GPL license).  If not, see <http://www.gnu.org/licenses/>.        */
+/*                                                                            */
+/* The copyright holder's institutional names and contributor's names may not */
+/* be used to endorse or promote products derived from this software without  */
+/* specific prior written permission of the institution or contributor.       */
+/******************************************************************************/
+
+#include <cerrno>
+
+#include "XrdCks/XrdCksXAttr.hh"
+#include "XrdCks/XrdCks.hh"
+#include "XrdOuc/XrdOucXAttr.hh"
+#include "XrdVersion.hh"
+
+
+#ifndef ENOATTR
+#define ENOATTR ENODATA
+#endif
+
+
+class MyXrdCksManager : public XrdCks
+{
+public:
+  MyXrdCksManager(XrdSysError *erP) : XrdCks(erP) {};
+
+  const char *Type(int &csSize) {return "adler32";}
+
+  /******************************************************************************/
+  /*                                   G e t                                    */
+  /******************************************************************************/
+
+  int Get(const char *Pfn, XrdCksData &Cks)
+  {
+     XrdOucXAttr<XrdCksXAttr> xCS;
+     int rc, nFault;
+  
+  // Determine which checksum to get (we will accept unsupported ones as well)
+  //
+     if (strncmp(Cks.Name, "adler32", 7) ) return -ENOTSUP;
+     if (!xCS.Attr.Cks.Set(Cks.Name)) return -ENOTSUP;
+  
+  // Retreive the attribute
+  //
+     if ((rc = xCS.Get(Pfn)) <= 0) return (rc && rc != -ENOATTR ? rc : -ESRCH);
+  
+  // Mark state of the name and copy the attribute over
+  //
+     nFault = strcmp(xCS.Attr.Cks.Name, Cks.Name);
+     Cks = xCS.Attr.Cks;
+  
+     return (nFault  ||  Cks.Length > XrdCksData::ValuSize || Cks.Length <= 0 ? -ESTALE : int(Cks.Length));
+  };
+
+  int Del(const char *Xfn, XrdCksData &Cks) {
+    return -ENOTSUP;
+  };
+
+  int        Calc( const char *Xfn, XrdCksData &Cks, int doSet=1) {
+    return -ENOTSUP;
+  };
+
+  int        Ver(  const char *Xfn, XrdCksData &Cks) {
+    return 0;
+  };
+
+  int Config(const char *Token, char *line) {
+    return 1;
+  };
+
+  int Init(const char *ConfigFN, const char *DfltCalc=0) {
+    return 1;
+  };
+
+  char * List(const char *Xfn, char *Buff, int Blen, char Sep=' ') {
+    return NULL;
+  };
+
+  const char *Name(int seqNum=0) {
+    return "adler32";
+  };
+
+  XrdCksCalc *Object(const char *name) {
+    return NULL;
+  };
+
+  int Size (const char *Name=0) {
+    return 4;
+  };
+
+  int Set(const char *Xfn, XrdCksData &Cks, int myTIme=0) {
+    return -ENOTSUP;
+  };
+};
+
+
+extern "C" MyXrdCksManager *XrdCksInit(XrdSysError *eDest,
+                                          const char  *csName,
+                                          const char  *cFN,
+                                          const char  *Parms) {
+  return new MyXrdCksManager(eDest);
+};
+
+XrdVERSIONINFO(XrdCksInit,"MyCksums-2");
